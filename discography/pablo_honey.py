a408f140e60f0a84d08592ee08f209341c2a7ae3
15Syncopation <75544572+15Syncopation@users.noreply.github.com>
2020-12-18 22:37:21 +0700

    Create pablo_honey.py

---
+++ discography/pablo_honey.py
@@ -0,0 +1,9 @@
+
+overview = '''Pablo Honey is the debut album by English rock band Radiohead. It was released on 22 February 1993 in the United Kingdom by Parlophone and in the United States by Capitol Records. It was produced by Sean Slade, Paul Q. Kolderie and Chris Hufford, and recorded at Chipping Norton Recording Studios in Oxfordshire from September to November 1992.
+
+Pablo Honey peaked at number 22 on the UK Albums Chart, and received generally favourable reviews, but some criticised its grunge sound as derivative and found certain songs underdeveloped. The album is often held in a negative light in comparison to the band's later work, although some retrospective reviews have been positive. Pablo Honey produced three charting singles – "Anyone Can Play Guitar", "Stop Whispering", and "Creep" – and was certified platinum in the United Kingdom and other countries.'''
+	
+release_info = {'Released date' : '22 Febuary 1993',
+				'Recorded' : 'September–November 1992',
+				'Studio' : 'Chipping Norton (Oxon)',
+				'Length' : '42:11'}
