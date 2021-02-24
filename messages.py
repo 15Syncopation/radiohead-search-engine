952f82603297f4510070bac18d65c13911774d92
15Syncopation <75544572+15Syncopation@users.noreply.github.com>
2021-02-24 07:19:21 +0700

    Create messages.py

---
+++ messages.py
@@ -0,0 +1,44 @@
+'''
+This file contain messages for 'main.py'.
+This messages will printed in the CLI.
+'''
+
+
+# Main Messages #
+
+welcome_message = '''
+Welcome to Radiohead Search Engine.
+In here you can read about their informations, watch their music videos, and listen to their songs.
+'''
+
+
+# Messages for the Help commands for the console #
+
+commands_HELP_message = '''
+List of available commands:
+
+READ [title] [help]			Reading a information about Radiohead's. Best practice is to use this command for reading about their material.
+WATCH [title] [help]		Watch the Radiohead songs. It could be music videos, or audio only.
+SHOW [title] [help]			Showing a available command for a song or album or discography or anything you type about their material.
+'''
+
+command_READ_HELP_message = '''
+READ [title] [help]
+
+Display information about Radiohead's discography/album, song, and history.
+
+ [title]		Display a information with the following attributes.
+ attributes			D	Discography/Album name.
+					S	Song(s) name.
+					H	History.
+					
+ [help]			Display the documentation for the typed command.
+'''
+
+command_WATCH_HELP_message = '''
+WATCH [title] [help]
+
+Open a new tab (or a new window) to the song(s) or album Youtube site.
+
+ [title]
+'''
