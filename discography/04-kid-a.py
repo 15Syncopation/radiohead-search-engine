bbe2d0242dd221cdc081693238adc14182238c00
15Syncopation <75544572+15Syncopation@users.noreply.github.com>
2021-02-24 07:26:10 +0700

    Update and rename kid-a.py to 04-kid-a.py

--- discography/04-kid-a.py
+++ discography/04-kid-a.py
@@ -1,5 +1,4 @@
 
-
 overview = '''Kid A is the fourth album by the English rock band Radiohead, released on 2 October 2000 by Parlophone. They recorded Kid A with OK Computer producer Nigel Godrich in Paris, Copenhagen, Gloucestershire and their hometown Oxford, England.
 
 After the stress of promoting Radiohead's acclaimed 1997 album OK Computer, songwriter Thom Yorke wanted to diverge from rock music. Drawing influence from electronic music, ambient music, krautrock, jazz, and 20th-century classical music, Radiohead used instruments such as modular synthesisers, ondes Martenot, brass and strings. They processed guitar sounds, incorporated samples and loops, and manipulated their recordings with software such as Pro Tools and Cubase. Yorke wrote many lyrics by cutting up words and phrases and assembling them at random. Radiohead considered releasing the material as a double album, but decided it was too dense; a second album of material from the sessions, Amnesiac, was released the following year.
@@ -8,14 +7,15 @@ Kid A was widely anticipated. In a departure from industry practice, Radiohead r
 
 Kid A debuted at the top of the UK Albums Chart, and became Radiohead's first number-one album in the United States, where it sold over 207,000 copies in its first week. It has been certified platinum in Australia, Canada, France, Japan, the US and the UK. Like OK Computer, it won the Grammy Award for Best Alternative Album and was nominated for the Grammy Award for Album of the Year. Its departure from Radiohead's earlier sound divided fans and critics, and some dismissed it as pretentious, deliberately obscure, or derivative. However, it later attracted wide acclaim; at the turn of the decade, Rolling Stone, Pitchfork and the Times ranked Kid A the greatest album of the 2000s. In 2020, Rolling Stone ranked it number 20 on its updated list of the 500 greatest albums of all time.'''
 
-release_info = {'Released date' : '',
-				'Recorded' : '',
-				'Studio' : '',
-				'Length' : '',
-				'Label' : '',
-				'Producer' : '',
-				'Singles' : ''}
-				
+
+release_info = {'Released date' : '2 October 2000',
+				'Recorded' : 'January 1999 - April 2000',
+				'Studio' : 'Guillaume Tell (Paris), Medley (Copenhagen), Unnamed studio (Oxford, England)',
+				'Length' : '49:56',
+				'Label' : 'Parlophone, Capitol',
+				'Producer' : 'Nigel Godrich, Radiohead'}
+
+
 track_listing = ['Everything in Its Right Place',
 				'Kid A',
 				'The National Anthem',
@@ -27,12 +27,20 @@ track_listing = ['Everything in Its Right Place',
 				'Morning Bell',
 				'Motion Picture Soundtrack']
 
+
 personel_band = {'Thom Yorke' : 'lead vocals, guitars',
 				'Jonny Greenwood' : 'guitar, electronic',
-				"Ed O'Brien" : 'guitar, backing vocals',
+				"Ed O'Brien" : "guitar, backing vocals",
 				'Colin Greenwood' : 'bass',
 				'Phil Selway' : 'drums'}
 
-personel_production = {'' : ''}
 
-personel_design = {'' : ''}
+personel_production = {'Nigel Godrich' : 'production, engineering, mixing',
+						'Radiohead' : 'production',
+						'Gerard Navarro' : 'production assistance, additional engineering',
+						'Graeme Stewart' : 'additional engineering',
+						'Chris Blair' : 'mastering'}
+
+
+personel_design = {'Stanley' : 'artwork ("Landscapes, Knives and Glue")',
+					'Tchock' : 'artwork ("Landscapes, Knives and Glue")'}
