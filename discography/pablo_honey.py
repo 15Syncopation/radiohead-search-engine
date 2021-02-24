f16b99631a0cbc5c7c69c8812a13d20a9cf5639c
15Syncopation <75544572+15Syncopation@users.noreply.github.com>
2021-02-24 07:19:50 +0700

    Update pablo_honey.py

--- discography/pablo_honey.py
+++ discography/pablo_honey.py
@@ -2,15 +2,38 @@
 overview = '''Pablo Honey is the debut album by English rock band Radiohead. It was released on 22 February 1993 in the United Kingdom by Parlophone and in the United States by Capitol Records. It was produced by Sean Slade, Paul Q. Kolderie and Chris Hufford, and recorded at Chipping Norton Recording Studios in Oxfordshire from September to November 1992.
 
 Pablo Honey peaked at number 22 on the UK Albums Chart, and received generally favourable reviews, but some criticised its grunge sound as derivative and found certain songs underdeveloped. The album is often held in a negative light in comparison to the band's later work, although some retrospective reviews have been positive. Pablo Honey produced three charting singles – "Anyone Can Play Guitar", "Stop Whispering", and "Creep" – and was certified platinum in the United Kingdom and other countries.'''
-	
+
+
+# Songs, Music Videos, and album link
+wikipedia_link = 'https://en.wikipedia.org/wiki/Pablo_Honey'
+songs_playlist = 'https://youtube.com/playlist?list=OLAK5uy_kogOCcCSJj35BNnHBOHHq4ueqasxkFP8g'
+songs_mv = {'Creep' : 'https://youtu.be/XFkzRNyygfk'}
+
+songs_playlist_accessable = {'You' : 'https://youtu.be/OzL7u5teZhg',
+							'Creep' : 'https://youtu.be/XFkzRNyygfk',
+							'How Do You?' : 'https://youtu.be/ukFTig7exNU',
+							'Stop Whispering' : 'https://youtu.be/DPpug5FUQsc',
+							'Thinking About You' : 'https://youtu.be/2jsPEOiMNaA',
+							'Anyone Can Play Guitar' : 'https://youtu.be/i_GZJLH0ynY',
+							'Ripcord' : 'https://youtu.be/i0GC7Oo_Zo4',
+							'Vegetable' : 'https://youtu.be/kALoYb7bepI',
+							'Prove Yourself' : 'https://youtu.be/o6Hqe1qKUhA',
+							"I Can't" : 'https://youtu.be/cwQZ3O3hUe8',
+							'Lurgee' : 'https://youtu.be/RGx5wxznSOE',
+							'Blow Out' : 'https://youtu.be/ocQImpRfPMk'}
+
+
+
+# Album info (Wikipedia)
 release_info = {'Released date' : '22 Febuary 1993',
-				'Recorded' : 'September–November 1992',
+				'Recorded' : 'September – November 1992',
 				'Studio' : 'Chipping Norton (Oxon)',
 				'Length' : '42:11',
 				'Label' : 'Parlophone, Capitol',
 				'Producer' : 'Sean Slade, Paul Q. Kolderie, Chris Hufford',
 				'Singles' : 'Creep (21 September 1992), Anyone Can Play Guitar (1 Febuary 1993), Stop Whispering (5 October 1993)'}
-				
+
+
 track_listing = ['You', 
 				'Creep', 
 				'How Do You?', 
@@ -24,25 +47,20 @@ track_listing = ['You',
 				'Lurgee', 
 				'Blow Out']
 
+
 personel_band = {'Colin Greenwood' : 'bass guitar',
 				'Jonny Greenwood' : 'lead guitar, piano, organ',
-				"Ed O'Brien" : 'guitar, vocals',
+				"Ed O'Brien" : "guitar, vocals",
 				'Phil Selway' : 'drums',
 				'Thom E. Yorke' : 'vocals, guitar'}
+
 
 personel_production = {'Chris Blair' : 'mastering',
 					'Chris Hufford' : 'production, engineering (tracks 10, 11)',
 					'Paul Q. Kolderie' : 'production, engineering (tracks 1–9, 12), mixing',
 					'Sean Slade' : 'production, engineering (tracks 1–9, 12), mixing'}
 
+
 personel_design = {'Icon' : 'design',
-					'Lisa Bunny Jones' : 'paintings',
-					'Tom Sheehan' : 'photography'}
-
-
-
-#ri = list(personel_band.keys())
-#print(ri)
-
-#for i in track_listing:
-#	print(i)
+				'Lisa Bunny Jones' : 'paintings',
+				'Tom Sheehan' : 'photography'}
