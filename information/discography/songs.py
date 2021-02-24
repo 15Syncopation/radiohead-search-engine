10835409cb6c73d42dd89e13fa48d7a23c1e7b53
15Syncopation <75544572+15Syncopation@users.noreply.github.com>
2021-02-24 07:28:54 +0700

    Update and rename discography/songs.py to information/discography/songs.py

---
+++ information/discography/songs.py
@@ -0,0 +1,34 @@
+'''
+This file contain songs.
+'''
+
+
+mv_list = {'Creep' :                            'https://youtu.be/XFkzRNyygfk',# Pablo Honey
+			'High and Dry' :                    'https://youtu.be/7qFfFVSerQo',###################
+			'Fake Plastic Trees' :              'https://youtu.be/n5h0qHwNrHk',#
+			'Just' :                            'https://youtu.be/oIFLtNYI3Ls',# The Bends
+			'My Iron Lung' : 					'https://youtu.be/pRU-6vaKaf4',#
+			'Street Spirit (Fade Out)' :        'https://youtu.be/LCJblaUkkfc',###################
+			'Paranoid Android' :				'https://youtu.be/fHiGbolFFGw',###################
+			'Karma Police' :                    'https://youtu.be/1uYWYWPc9HU',#
+            'No Suprises' :                     'https://youtu.be/u5CVsCnxyXg',# OK
+            'I Promise' :                       'https://youtu.be/0sFvFVkeGVg',# Computer
+            'Man Of War' :                      'https://youtu.be/DXP1KdZX4io',#
+            'Lift' :                            'https://youtu.be/QBGaO89cBMI',###################
+            'Pyramid Song' :                    'https://youtu.be/3M_Gg1xAHE4',###################
+            'Push Pulk / Spinning Plates' :     'https://youtu.be/ih2Ftq3hJoI',# Amnesiac
+            'I Might Be Wrong' :                'https://youtu.be/vOa--Dhu11M',#
+            'Knives Out' :                      'https://youtu.be/2Lpw3yMCWro',###################
+            'Sit Down Stand Up' :               'https://youtu.be/CVf_HGoY-1E',###################
+            'Go To Sleep' :                     'https://youtu.be/Fe6X9fLLp0Y',# Hail to the Thief
+            'There, There' :                    'https://youtu.be/7AQSLozK7aA',###################
+            '15 Step' :							'https://youtu.be/xpqk9MD6vLM',###################
+            'Nude' :                            'https://youtu.be/BbWBRnDK_AE',#
+            'Weird Fishes' :					'https://youtu.be/TNRCvG9YtYI',#
+            'All I Need' :						'https://youtu.be/DV1hQSt2hSE',# In Rainbows
+            'Rockoner':							'https://youtu.be/_uofQD-N6UI',#
+            'House of Cards' :                  'https://youtu.be/8nTFjVm9sTQ',#
+            'Jigsaw Falling Into Place' :       'https://youtu.be/GoLJJRIWCLU',###################
+            'Lotus Flower' :                    'https://youtu.be/cfOa1a8hYP8',###################
+            'Burn The Witch' :                  'https://youtu.be/yI2oS2hoL0k',# A Moon
+            'Daydreaming' :                     'https://youtu.be/TTAU7lLDZYU'}# Shaped Pool
